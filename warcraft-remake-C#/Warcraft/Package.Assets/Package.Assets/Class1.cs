2d15ed3e3c85aada587ce68faa39dce9ec7cb35f
PantsStatusZero <cordovanick@yahoo.com>
2015-08-26 18:17:29 -0500

    Add Package.Assets Code Lib and moved RawAssets
    Add a Code Lib to Package.Assets and moved RawAssets to Paradox Package.

---
+++ warcraft-remake-C#/Warcraft/Package.Assets/Package.Assets/Class1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Paradox;
+using SiliconStudio.Paradox.Rendering;
+using SiliconStudio.Paradox.Engine;
+using SiliconStudio.Paradox.Graphics;
+
+namespace Package.Assets
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+
+        }
+    }
+}
